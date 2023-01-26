d21ede45014a3d100e6daf9d08ded50ca8b90fb6
LuisMi-CAfuera <luibra861@alu.centroafuera.es>
2023-01-26 13:42:06 +0100

    9

--- app/src/main/java/com/example/ejercicio7/Ejercicio10.kt
+++ app/src/main/java/com/example/ejercicio7/Ejercicio10.kt
@@ -2,10 +2,15 @@ package com.example.ejercicio7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ejercicio7.databinding.ActivityMain4Binding
 
 class Ejercicio10 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain4Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main4)
+        binding = ActivityMain4Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
