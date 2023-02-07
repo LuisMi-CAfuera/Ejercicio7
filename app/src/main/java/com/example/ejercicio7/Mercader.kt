4036c625c4dbe39a7c828e0cd6316cc2e15fab6e
LuisMi-CAfuera <113511965+LuisMi-CAfuera@users.noreply.github.com>
2023-02-07 13:53:12 +0100

    18

--- app/src/main/java/com/example/ejercicio7/Mercader.kt
+++ app/src/main/java/com/example/ejercicio7/Mercader.kt
@@ -1,8 +1,10 @@
 package com.example.ejercicio7
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.ejercicio7.databinding.ActivityMercaderBinding
 import com.google.gson.Gson
 
@@ -12,26 +14,35 @@ class Mercader : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMercaderBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val shared= getSharedPreferences("Personaje", Context.MODE_PRIVATE)
         val gson = Gson()
-        val json = intent.getStringExtra("Personaje")
+        var json = shared.getString("Personaje", "")
         val p = gson.fromJson(json, Personaje::class.java)
 
 
-        binding.Mercader.setImageResource(R.drawable.mercader)
+
 
 
 
         binding.Comerciar.setOnClickListener{
-            val intent = Intent(this@Mercader, Comerciar::class.java)
-            val json2 = gson.toJson(p)
-            intent.putExtra("Personaje", json2)
-            startActivity(intent)
+            binding.Comerciar.visibility = View.INVISIBLE
+            binding.Comprar.visibility = View.VISIBLE
+            binding.Vender.visibility = View.VISIBLE
+            binding.Cancelar.visibility = View.VISIBLE
+            binding.Volver.visibility = View.INVISIBLE
+        }
+
+        binding.Comprar.setOnClickListener{
+            binding.Mercader.setImageResource()
         }
 
         binding.Volver.setOnClickListener{
             val intent = Intent(this@Mercader, Ejercicio10::class.java)
-            val json2 = gson.toJson(p)
-            intent.putExtra("Personaje", json2)
+            val edit = shared.edit()
+            edit.clear()
+            json = gson.toJson(p)
+            edit.putString("Personaje", json)
+            edit.apply()
             startActivity(intent)
         }
 
