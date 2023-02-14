89da54341558ea1189bcd2796d1b97c2e33fd2ae
LuisMi-CAfuera <113511965+LuisMi-CAfuera@users.noreply.github.com>
2023-02-14 13:00:59 +0100

    25

--- app/src/main/java/com/example/ejercicio7/DbHelper.kt
+++ app/src/main/java/com/example/ejercicio7/DbHelper.kt
@@ -12,10 +12,10 @@ class DbHelper (context: Context) : SQLiteOpenHelper(context, DATABASE_NAME,null
 
     companion object {
         private const val DATABASE_NAME = "personajes.db"
-        private const val TABLE_PERSONAJE = "personajes"
-        private const val TABLE_MOCHILA = "mochila"
-        private const val TABLE_OBJETOS = "objetos"
         private const val DATABASE_VERSION = 1
+        //Tabla Personaje
+        private const val TABLE_PERSONAJE = "personajes"
+        private const val ID_PERSONAJE = "id_personaje"
         private const val NOMBRE = "nombre"
         private const val ESTADO_VITAL = "estadoVital"
         private const val RAZA = "raza"
@@ -29,22 +29,39 @@ class DbHelper (context: Context) : SQLiteOpenHelper(context, DATABASE_NAME,null
         private const val CARISMA = "carisma"
         private const val VIDA = "vida"
         private const val id_mochila = "id_mochila"
-        private const val MONEDERO = "monedero"
-        private const val createTable = "CREATE TABLE $TABLE_PERSONAJE (" +
-                "$NOMBRE TEXT PRIMARY KEY," +
-                "$ESTADO_VITAL TEXT," +
-                "$RAZA TEXT," +
-                "$CLASE TEXT," +
-                "$FUERZA INTEGER," +
-                "$DESTREZA INTEGER," +
-                "$DEFENSA INTEGER," +
-                "$INTELIGENCIA INTEGER," +
-                "$CONSTITUCION INTEGER," +
-                "$SABIDURIA INTEGER," +
-                "$CARISMA INTEGER," +
-                "$VIDA INTEGER," +
-                "$id_mochila TEXT," +
-                "$MONEDERO TEXT)"
+        private const val id_monedero = "id_monedero"
+
+
+        //Tabla Mochila
+        private const val TABLE_MOCHILA = "mochila"
+        private const val mochila_id = "mochila_id"
+        private const val objeto_id = "id_objeto"
+        private const val PESO_MOCHILA = "peso_mochila"
+        private const val PESO_MAXIMO = "peso_maximo"
+
+        //Tabla Monedero
+        private const val TABLE_MONEDERO = "monedero"
+        private const val monedero_id = "monedero_id"
+        private const val personaje_id = "personaje_id"
+        private const val uno = "1"
+        private const val diez = "10"
+        private const val veinte = "20"
+        private const val cincuenta = "50"
+        private const val cien = "100"
+
+
+        //Tabla de Objetos aleatorios
+        private const val TABLE_OBJETOS_ALEATORIOS = "objetos_aleatorios"
+        private const val id_objeto = "id_objeto"
+        private const val NOMBRE_OBJETO = "nombre_objeto"
+        private const val PESO = "peso"
+        private const val VALOR = "valor"
+        private const val VIDA_OBJETO = "vida_objeto"
+        private const val DANIO = "danio"
+        private const val RECUPERACION = "recuperacion"
+        private const val DEFENSA_OBJETO = "defensa_objeto"
+        private const val AUMNETO_HP = "aumento_hp"
+
     }
 
 
