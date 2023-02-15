6db6d34fe90694dde13b12504ed89d34256ddb18
LuisMi-CAfuera <113511965+LuisMi-CAfuera@users.noreply.github.com>
2023-02-15 08:47:50 +0100

    26

--- app/src/main/java/com/example/ejercicio7/DbHelper.kt
+++ app/src/main/java/com/example/ejercicio7/DbHelper.kt
@@ -13,6 +13,60 @@ class DbHelper (context: Context) : SQLiteOpenHelper(context, DATABASE_NAME,null
     companion object {
         private const val DATABASE_NAME = "personajes.db"
         private const val DATABASE_VERSION = 1
+
+
+        //Tabla de Objetos aleatorios
+        private const val TABLE_OBJETOS_ALEATORIOS = "objetos_aleatorios"
+        private const val ID_OBJETO = "id_objeto"
+        private const val NOMBRE_OBJETO = "nombre_objeto"
+        private const val PESO = "peso"
+        private const val VALOR = "valor"
+        private const val VIDA_OBJETO = "vida_objeto"
+        private const val DANIO = "danio"
+        private const val RECUPERACION = "recuperacion"
+        private const val DEFENSA_OBJETO = "defensa_objeto"
+        private const val AUMNETO_HP = "aumento_hp"
+        private const val CREATE_TABLE_OBJETOS_ALEATORIOS = ("CREATE TABLE " + TABLE_OBJETOS_ALEATORIOS + "("
+                + ID_OBJETO + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + NOMBRE_OBJETO + " TEXT,"
+                + PESO + " INTEGER,"
+                + VALOR + " INTEGER,"
+                + VIDA_OBJETO + " INTEGER,"
+                + DANIO + " INTEGER,"
+                + RECUPERACION + " INTEGER,"
+                + DEFENSA_OBJETO + " INTEGER,"
+                + AUMNETO_HP + " INTEGER"
+                + ")")
+
+        //Tabla Mochila
+        private const val TABLE_MOCHILA = "mochila"
+        private const val MOCHILA_ID = "id_mochila"
+        private const val OBJETO_ID = "id_objeto"
+        private const val PESO_MOCHILA = "peso_mochila"
+        private const val PESO_MAXIMO = "peso_maximo"
+        private const val CREATE_TABLE_MOCHILA = ("CREATE TABLE " + TABLE_MOCHILA + "("
+                + MOCHILA_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + OBJETO_ID + " INTEGER,"
+                + PESO_MOCHILA + " INTEGER,"
+                + PESO_MAXIMO + " INTEGER,"
+                + "FOREIGN KEY (" + OBJETO_ID + ") REFERENCES " + TABLE_OBJETOS_ALEATORIOS + "(" + ID_OBJETO + ")"
+                + ")")
+        //Tabla Monedero
+        private const val TABLE_MONEDERO = "monedero"
+        private const val monedero_id = "id_monedero"
+        private const val UNO = "1"
+        private const val DIEZ = "10"
+        private const val VEINTE = "20"
+        private const val CINCUENTA = "50"
+        private const val CIEN = "100"
+        private const val CREATE_TABLE_MONEDERO = ("CREATE TABLE " + TABLE_MONEDERO + "("
+                + monedero_id + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + UNO + " INTEGER,"
+                + DIEZ + " INTEGER,"
+                + VEINTE + " INTEGER,"
+                + CINCUENTA + " INTEGER,"
+                + CIEN + " INTEGER"
+                + ")")
         //Tabla Personaje
         private const val TABLE_PERSONAJE = "personajes"
         private const val ID_PERSONAJE = "id_personaje"
@@ -28,49 +82,43 @@ class DbHelper (context: Context) : SQLiteOpenHelper(context, DATABASE_NAME,null
         private const val SABIDURIA = "sabiduria"
         private const val CARISMA = "carisma"
         private const val VIDA = "vida"
-        private const val id_mochila = "id_mochila"
-        private const val id_monedero = "id_monedero"
-
-
-        //Tabla Mochila
-        private const val TABLE_MOCHILA = "mochila"
-        private const val mochila_id = "id_mochila"
-        private const val objeto_id = "id_objeto"
-        private const val PESO_MOCHILA = "peso_mochila"
-        private const val PESO_MAXIMO = "peso_maximo"
-
-        //Tabla Monedero
-        private const val TABLE_MONEDERO = "monedero"
-        private const val monedero_id = "id_monedero"
-        private const val personaje_id = "personaje_id"
-        private const val uno = "1"
-        private const val diez = "10"
-        private const val veinte = "20"
-        private const val cincuenta = "50"
-        private const val cien = "100"
-
-
-        //Tabla de Objetos aleatorios
-        private const val TABLE_OBJETOS_ALEATORIOS = "objetos_aleatorios"
-        private const val id_objeto = "id_objeto"
-        private const val NOMBRE_OBJETO = "nombre_objeto"
-        private const val PESO = "peso"
-        private const val VALOR = "valor"
-        private const val VIDA_OBJETO = "vida_objeto"
-        private const val DANIO = "danio"
-        private const val RECUPERACION = "recuperacion"
-        private const val DEFENSA_OBJETO = "defensa_objeto"
-        private const val AUMNETO_HP = "aumento_hp"
+        private const val ID_MOCHILA = "id_mochila"
+        private const val ID_MONEDERO = "id_monedero"
+        private const val CREATE_TABLE_PERSONAJE = ("CREATE TABLE " + TABLE_PERSONAJE + "("
+                + ID_PERSONAJE + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + NOMBRE + " TEXT,"
+                + ESTADO_VITAL + " TEXT,"
+                + RAZA + " TEXT,"
+                + CLASE + " TEXT,"
+                + FUERZA + " INTEGER,"
+                + DESTREZA + " INTEGER,"
+                + DEFENSA + " INTEGER,"
+                + INTELIGENCIA + " INTEGER,"
+                + CONSTITUCION + " INTEGER,"
+                + SABIDURIA + " INTEGER,"
+                + CARISMA + " INTEGER,"
+                + VIDA + " INTEGER,"
+                + ID_MOCHILA + " INTEGER,"
+                + ID_MONEDERO + " INTEGER,"
+                + "FOREIGN KEY (" + ID_MOCHILA + ") REFERENCES " + TABLE_MOCHILA + "(" + MOCHILA_ID + "),"
+                + "FOREIGN KEY (" + ID_MONEDERO + ") REFERENCES " + TABLE_MONEDERO + "(" + monedero_id + ")"
+                + ")")
 
     }
 
 
     override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL(createTable)
+        db.execSQL(CREATE_TABLE_OBJETOS_ALEATORIOS)
+        db.execSQL(CREATE_TABLE_MOCHILA)
+        db.execSQL(CREATE_TABLE_MONEDERO)
+        db.execSQL(CREATE_TABLE_PERSONAJE)
 
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_OBJETOS_ALEATORIOS")
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_MOCHILA")
+        db.execSQL("DROP TABLE IF EXISTS $TABLE_MONEDERO")
         db.execSQL("DROP TABLE IF EXISTS $TABLE_PERSONAJE")
         onCreate(db)
 
